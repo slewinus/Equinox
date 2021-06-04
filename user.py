f2e6f993b937c5ba676f0925ca1218e46446adbe
Elliott-lvnb <elliott.loevenbruck@gmail.com>
2021-06-04 02:09:53 +0200

    Like profil

--- user.py
+++ user.py
@@ -1,3 +1,5 @@
+from like import *
+
 class User:
     def __init__(self, n, p, f, last, pp, b, i):
         self.username = n
@@ -39,10 +41,18 @@ def get_user_posts(user_id, db, first_post, last_post):
     cursor.execute('SELECT * FROM communities ORDER BY id')
     comms = cursor.fetchall()
     comms = [Subquinox(c[0], c[1], c[2]) for c in comms]
-    posts = [Submission(User(p[10], p[11], p[12], p[13], p[14], p[15], p[9]), p[0], p[2], p[3], p[4] if p[4] != '' else None, p[5], p[6], comms[p[7]], p[8]) for p in result]
-    posts.sort()
-    posts.reverse()
-    return posts[first_post:last_post]
+    result = [Submission(User(p[10], p[11], p[12], p[13], p[14], p[15], p[9]), p[0], p[2], p[3], p[4] if p[4] != '' else None, p[5], p[6], comms[p[7] - 1], p[8]) for p in result]
+    posts = []
+    result.sort()
+    result.reverse()
+    for post in result[first_post:last_post]:
+        if is_post_liked(user_id, post.get_id(), db):
+            posts.append((post, 'liked'))
+        elif is_post_disliked(user_id, post.get_id(), db):
+            posts.append((post, 'disliked'))
+        else:
+            posts.append((post, None))
+    return posts
 
 
 def get_user_from_id(user_id, db):
